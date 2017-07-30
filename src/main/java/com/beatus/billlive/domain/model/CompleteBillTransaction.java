28bb4a01a93f3d7721201434078f763da6a60a22
Akey, Veera Venkata Abhinav <abhinav.akey@teamaol.com>
2017-07-30 01:36:18 -0400

    changes to completeBillTransaction

--- src/main/java/com/beatus/billlive/domain/model/CompleteBillTransaction.java
+++ src/main/java/com/beatus/billlive/domain/model/CompleteBillTransaction.java
@@ -1,10 +1,13 @@
 package com.beatus.billlive.domain.model;
 
-public class CompleteBillTransaction {
+public class CompleteBillTransaction extends BaseData{
 	private String billNumber;
-	private String totalAmount;
-	private String amountPaid;
-	private String amountDue;
+	private String postId;
+	private Double totalAmount;
+	private String date;
+	private String reference;
+	private Double amountPaid;
+	private Double amountDue;
 	
 	public String getBillNumber() {
 		return billNumber;
@@ -12,22 +15,40 @@ public class CompleteBillTransaction {
 	public void setBillNumber(String billNumber) {
 		this.billNumber = billNumber;
 	}
-	public String getTotalAmount() {
+	public String getPostId() {
+		return postId;
+	}
+	public void setPostId(String postId) {
+		this.postId = postId;
+	}
+	public Double getTotalAmount() {
 		return totalAmount;
 	}
-	public void setTotalAmount(String totalAmount) {
+	public void setTotalAmount(Double totalAmount) {
 		this.totalAmount = totalAmount;
 	}
-	public String getAmountPaid() {
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getReference() {
+		return reference;
+	}
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+	public Double getAmountPaid() {
 		return amountPaid;
 	}
-	public void setAmountPaid(String amountPaid) {
+	public void setAmountPaid(Double amountPaid) {
 		this.amountPaid = amountPaid;
 	}
-	public String getAmountDue() {
+	public Double getAmountDue() {
 		return amountDue;
 	}
-	public void setAmountDue(String amountDue) {
+	public void setAmountDue(Double amountDue) {
 		this.amountDue = amountDue;
 	}
 }
